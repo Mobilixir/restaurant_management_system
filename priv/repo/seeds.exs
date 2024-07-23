96407c117afb55c674bcfccef22b59976503f448
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2024-07-23 21:19:41 +0530

    - Updated seeds data

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,46 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     Resman.Repo.insert!(%Resman.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+alias Resman.Accounts
+
+Accounts.register_user(%{
+  email: "user@mobilixir.com",
+  password: "User@Mobilixir11",
+  first_name: "User",
+  last_name: "Mobilixir",
+  user_name: "user",
+  role: "USER"
+})
+
+Accounts.register_user(%{
+  email: "admin@mobilixir.com",
+  password: "Admin@Mobilixir11",
+  first_name: "Admin",
+  last_name: "Mobilixir",
+  user_name: "admin",
+  role: "ADMIN"
+})
+
+Accounts.register_user(%{
+  email: "captain@mobilixir.com",
+  password: "Captain@Mobilixir11",
+  first_name: "Captain",
+  last_name: "Mobilixir",
+  user_name: "captain",
+  role: "CAPTAIN"
+})
+
+Accounts.register_user(%{
+  email: "captain@mobilixir.com",
+  password: "Captain@Mobilixir11",
+  first_name: "Captain_One",
+  last_name: "Mobilixir",
+  user_name: "captain_one",
+  role: "CAPTAIN"
+})
+
+Accounts.register_user(%{
+  email: "captain_two@mobilixir.com",
+  password: "Captain@Mobilixir11",
+  first_name: "Captain_Two",
+  last_name: "Mobilixir",
+  user_name: "captain_two",
+  role: "CAPTAIN"
+})

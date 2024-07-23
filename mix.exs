c18f851f5d3a1c3f7201b146b0ee2415eb410435
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2024-07-23 21:12:20 +0530

    - Added argon2, bcrypt, credo, excoveralls, sobelow, dialyxir, waffle

--- mix.exs
+++ mix.exs
@@ -9,7 +9,15 @@ defmodule Resman.MixProject do
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test,
+        "coveralls.cobertura": :test
+      ]
     ]
   end
 
@@ -32,6 +40,7 @@ defmodule Resman.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:argon2_elixir, "~> 3.0"},
       {:phoenix, "~> 1.7.14"},
       {:phoenix_ecto, "~> 4.5"},
       {:ecto_sql, "~> 3.10"},
@@ -39,7 +48,7 @@ defmodule Resman.MixProject do
       {:phoenix_html, "~> 4.1"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       # TODO bump on release to {:phoenix_live_view, "~> 1.0.0"},
-      {:phoenix_live_view, "~> 1.0.0-rc.1", override: true},
+      {:phoenix_live_view, "~> 0.20.2"},
       {:floki, ">= 0.30.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.8.3"},
       {:esbuild, "~> 0.8", runtime: Mix.env() == :dev},
@@ -59,6 +68,14 @@ defmodule Resman.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.5"},
+      {:bcrypt_elixir, "~> 3.0"},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.18", only: :test},
+      {:sobelow, "~> 0.13", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
+      {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false},
+      {:waffle, "~> 1.1"},
+      {:waffle_ecto, "~> 0.0.11"},
       {:live_view_native, github: "liveview-native/live_view_native", branch: "main", override: true},
       {:live_view_native_stylesheet, github: "liveview-native/live_view_native_stylesheet", branch: "main"},
       {:live_view_native_swiftui, github: "liveview-native/liveview-client-swiftui", branch: "main"},

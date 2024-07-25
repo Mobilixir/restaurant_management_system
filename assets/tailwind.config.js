502f62d74234d8086ebd2142b121cffede796704
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2024-07-25 18:00:17 +0530

    - Added animation in tailwind css config

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -15,7 +15,22 @@ module.exports = {
     extend: {
       colors: {
         brand: "#FD4F00",
-      }
+      },
+      keyframes: {
+        slidein: {
+          from: {
+            opacity: "0",
+            transform: "translateY(-10px)",
+          },
+          to: {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+      },
+      animation: {
+        slidein: "slidein 1s ease var(--slidein-delay, 0) forwards",
+      },
     },
   },
   plugins: [

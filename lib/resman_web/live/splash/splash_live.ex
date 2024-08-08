145e59bb4845f5c79d5d24f1fde23818fd9ab25c
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2024-08-08 17:43:30 +0530

    - Updated animation values

--- lib/resman_web/live/splash/splash_live.ex
+++ lib/resman_web/live/splash/splash_live.ex
@@ -14,15 +14,17 @@ defmodule ResmanWeb.SplashLive do
       <img
         src={~p"/images/splash/splash_bg_left.svg"}
         alt=""
-        class="h-full w-2/6 animate-slidein opacity-0 [--slidein-delay:300ms]"
+        class="h-full w-2/6 animate-slidein opacity-0 [--slidein-delay:400ms]"
       />
       <img
         src={~p"/images/splash/splash_bg_right.svg"}
         alt=""
-        class="h-full w-2/6 animate-slidein opacity-0 [--slidein-delay:500ms]"
+        class="h-full w-2/6 animate-slidein opacity-0 [--slidein-delay:800ms]"
       />
-      <div class="grid h-screen w-screen place-items-center absolute animate-slidein opacity-0 [--slidein-delay:700ms]">
-        <p class="font-mono text-5xl">Resman</p>
+      <div class="flex h-screen w-screen justify-center place-items-center absolute animate-slidein opacity-0 [--slidein-delay:1600ms]">
+        <p class="font-mono text-5xl px-1">
+          Res<p class="text-[#FF7B2C] font-mono text-5xl px-1">Man</p>
+        </p>
       </div>
     </div>
     """
@@ -30,7 +32,9 @@ defmodule ResmanWeb.SplashLive do
 
   @impl true
   def mount(_params, _session, socket) do
-    # Process.send_after(self(), :clear_flash, 3000)
+    IO.inspect(socket, label: "*****")
+    
+    Process.send_after(self(), :clear_flash, 3000)
     {:ok, socket}
   end
 
